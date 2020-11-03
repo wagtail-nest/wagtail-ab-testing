e7272a3531aa244fcef0bb9752d3139bde66a3df
Karl Hobley <karl@kaed.uk>
2020-11-03 16:01:29 +0000

    Remove trailing whitespace

--- wagtail_ab_testing/wagtail_hooks.py
+++ wagtail_ab_testing/wagtail_hooks.py
@@ -8,7 +8,7 @@ from wagtail.core import hooks
 def register_admin_urls():
     urls = [
         path('jsi18n/', JavaScriptCatalog.as_view(packages=['wagtail_ab_testing']), name='javascript_catalog'),
-        
+
         # Add your other URLs here, and they will appear under `/admin/ab_testing/`
         # Note: you do not need to check for authentication in views added here, Wagtail does this for you!
     ]
