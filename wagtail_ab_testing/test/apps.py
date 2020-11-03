56a3ae267015f6973a5095826bb9e66c8636fd2f
Karl Hobley <karl@kaed.uk>
2020-11-03 15:55:09 +0000

    Fix verbose name of tests app

--- wagtail_ab_testing/test/apps.py
+++ wagtail_ab_testing/test/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class WagtailAbTestingTestAppConfig(AppConfig):
     label = "wagtail_ab_testing_test"
     name = "wagtail_ab_testing.test"
-    verbose_name = "A/B Testing tests"
+    verbose_name = "Wagtail A/B Testing tests"
