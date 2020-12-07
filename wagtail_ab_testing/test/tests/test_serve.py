5b4fcc2339f63b42407f4fd7581ddd8a928b7935
Karl Hobley <karl@torchbox.com>
2020-12-07 12:56:34 +0000

    Add completion logic for "Visit Page" goal

--- wagtail_ab_testing/test/tests/test_serve.py
+++ wagtail_ab_testing/test/tests/test_serve.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum, Q
 from django.test import TestCase
 from wagtail.core.models import Page
 
@@ -13,8 +14,10 @@ class TestServe(TestCase):
             page=self.home_page,
             name="Test",
             variant_revision=revision,
-            goal_event="foo",
+            goal_event="visit-page",
+            goal_page=self.home_page.add_child(instance=Page(title="Goal", slug="goal")),
             sample_size=10,
+            status=AbTest.STATUS_RUNNING,
         )
 
     def test_serves_control(self):
@@ -42,6 +45,22 @@ class TestServe(TestCase):
         self.assertContains(response, "Changed title")
 
         self.assertEqual(self.client.session[f'wagtail-ab-testing_{self.ab_test.id}_version'], AbTest.VERSION_VARIANT)
+
+    def test_serves_control_when_paused(self):
+        self.ab_test.status = AbTest.STATUS_PAUSED
+        self.ab_test.save()
+
+        # Add a participant for control
+        # This time, the next viewer will still see the control as the test is not running
+        self.ab_test.add_participant(AbTest.VERSION_CONTROL)
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "Welcome to your new Wagtail site!")
+        self.assertNotContains(response, "Changed title")
+
+        self.assertNotIn(f'wagtail-ab-testing_{self.ab_test.id}_version', self.client.session)
 
     def test_doesnt_track_bots(self):
         # Add a participant for control
@@ -71,3 +90,52 @@ class TestServe(TestCase):
         self.assertContains(response, "Welcome to your new Wagtail site!")
         self.assertNotContains(response, "Changed title")
         self.assertNotIn(f'wagtail-ab-testing_{self.ab_test.id}_version', self.client.session)
+
+    def test_visit_page_goal_completion(self):
+        session = self.client.session
+        session[f'wagtail-ab-testing_{self.ab_test.id}_version'] = 'variant'
+        session.save()
+
+        response = self.client.get('/goal/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(self.client.session[f'wagtail-ab-testing_{self.ab_test.id}_completed'], 'yes')
+
+        stats = self.ab_test.hourly_logs.aggregate(
+            control_conversions=Sum('conversions', filter=Q(version=AbTest.VERSION_CONTROL)),
+            variant_conversions=Sum('conversions', filter=Q(version=AbTest.VERSION_VARIANT)),
+        )
+
+        self.assertEqual(stats['control_conversions'] or 0, 0)
+        self.assertEqual(stats['variant_conversions'] or 0, 1)
+
+    def test_visit_page_goal_completion_doesnt_count_second_time(self):
+        # Shouldn't be counted if test marked as completed in users session
+        session = self.client.session
+        session[f'wagtail-ab-testing_{self.ab_test.id}_version'] = 'variant'
+        session[f'wagtail-ab-testing_{self.ab_test.id}_completed'] = 'yes'
+        session.save()
+
+        response = self.client.get('/goal/')
+        self.assertEqual(response.status_code, 200)
+
+        stats = self.ab_test.hourly_logs.aggregate(
+            control_conversions=Sum('conversions', filter=Q(version=AbTest.VERSION_CONTROL)),
+            variant_conversions=Sum('conversions', filter=Q(version=AbTest.VERSION_VARIANT)),
+        )
+
+        self.assertEqual(stats['control_conversions'] or 0, 0)
+        self.assertEqual(stats['variant_conversions'] or 0, 0)
+
+    def test_visit_page_goal_completion_doesnt_count_if_not_participant(self):
+        # Shouldn't be counted if the user is not a participant
+        response = self.client.get('/goal/')
+        self.assertEqual(response.status_code, 200)
+
+        stats = self.ab_test.hourly_logs.aggregate(
+            control_conversions=Sum('conversions', filter=Q(version=AbTest.VERSION_CONTROL)),
+            variant_conversions=Sum('conversions', filter=Q(version=AbTest.VERSION_VARIANT)),
+        )
+
+        self.assertEqual(stats['control_conversions'] or 0, 0)
+        self.assertEqual(stats['variant_conversions'] or 0, 0)
