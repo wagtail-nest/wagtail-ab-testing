0562b9f4d8889232283c989d066e7f829aecd5d0
Karl Hobley <karl@torchbox.com>
2020-12-11 15:55:34 +0000

    Show help box when there are no A/B tests

--- wagtail_ab_testing/static_src/components/PageEditorTab/index.tsx
+++ wagtail_ab_testing/static_src/components/PageEditorTab/index.tsx
@@ -14,34 +14,62 @@ interface PageEditorTabProps {
 }
 
 const PageEditorTab: FunctionComponent<PageEditorTabProps> = ({ tests }) => {
-    return (
-        <div className="nice-padding">
-            <table className="listing">
-                <thead>
-                    <tr>
-                        <th className="title">{gettext('Started at')}</th>
-                        <th>{gettext('Test name')}</th>
-                        <th>{gettext('Status')}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tests.map(test => (
-                        <tr key={test.id}>
-                            <td className="title">{test.started_at}</td>
-                            <td>
-                                <a href={test.results_url}>{test.name}</a>
-                            </td>
-                            <td>
-                                <span className="status-tag primary">
-                                    {test.status}
-                                </span>
-                            </td>
+    if (tests.length > 0) {
+        return (
+            <div className="nice-padding">
+                <table className="listing">
+                    <thead>
+                        <tr>
+                            <th className="title">{gettext('Started at')}</th>
+                            <th>{gettext('Test name')}</th>
+                            <th>{gettext('Status')}</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+                    </thead>
+                    <tbody>
+                        {tests.map(test => (
+                            <tr key={test.id}>
+                                <td className="title">{test.started_at}</td>
+                                <td>
+                                    <a href={test.results_url}>{test.name}</a>
+                                </td>
+                                <td>
+                                    <span className="status-tag primary">
+                                        {test.status}
+                                    </span>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    } else {
+        return (
+            <div className="nice-padding">
+                <p className="help-block help-info">
+                    {gettext('To create an A/B test:')}
+                    <ol>
+                        <li style={{ listStyleType: 'decimal' }}>
+                            {gettext(
+                                'Ensure you have a published version of the page (this will act as the "Control" in your test)'
+                            )}
+                        </li>
+                        <li style={{ listStyleType: 'decimal' }}>
+                            {gettext(
+                                'Make changes to the page content (this will act as the "Variant" in your test) and choose "SAVE AND CREATE A/B TEST"'
+                            )}
+                        </li>
+                        <li style={{ listStyleType: 'decimal' }}>
+                            {gettext('Choose your goal')}
+                        </li>
+                        <li style={{ listStyleType: 'decimal' }}>
+                            {gettext('Watch your test results come in')}
+                        </li>
+                    </ol>
+                </p>
+            </div>
+        );
+    }
 };
 
 export function initPageEditorTab(element: HTMLElement, props: any) {
