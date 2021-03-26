449a237292d401dea4a9dfe0ee49381cf5a049b4
Karl Hobley <karl@torchbox.com>
2021-03-26 16:53:12 +0000

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name="wagtail-ab-testing",
-    version="0.1.2",
+    version="0.2",
     description="A/B Testing for Wagtail",
     author="Karl Hobley",
     author_email="karl@torchbox.com",
