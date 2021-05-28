1132e1c930a1dd5858de059bef2cd2e3f2089faa
Kalob Taulien <kalob.taulien@gmail.com>
2021-05-28 10:20:30 -0600

    Version bump to 0.2.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name="wagtail-ab-testing",
-    version="0.2.1",
+    version="0.2.2",
     description="A/B Testing for Wagtail",
     author="Karl Hobley",
     author_email="karl@torchbox.com",
