58fa6e826af17d2f712f5b2d9053aa7cb99508f2
Nick Moreton <nick.moreton@torchbox.com>
2021-09-01 16:53:57 +0100

    allow wagtail 2.14

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     ],
     install_requires=[
         "Django>=2.2,<3.3",
-        "Wagtail>=2.11,<2.14",
+        "Wagtail>=2.11,<2.15",
         "user-agents>=2.2,<2.3",
         "numpy>=1.19.4,<1.20",
         "scipy>=1.5.4,<1.6",
