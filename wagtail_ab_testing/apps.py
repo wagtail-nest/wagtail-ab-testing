54e04efe0f47028cd246404afb6d0a53b4f04f3e
Dan Braghis <dan.braghis@torchbox.com>
2021-10-29 10:46:00 +0100

    Add default_auto_field to app config

--- wagtail_ab_testing/apps.py
+++ wagtail_ab_testing/apps.py
@@ -5,3 +5,4 @@ class WagtailAbTestingAppConfig(AppConfig):
     label = "wagtail_ab_testing"
     name = "wagtail_ab_testing"
     verbose_name = "A/B Testing"
+    default_auto_field = "django.db.models.AutoField"
