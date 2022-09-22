3d437d4b9620c04fe5e523ee02b8e165d215786a
nick.moreton <nick.moreton@torchbox.com>
2022-09-22 11:03:16 +0100

    Be more consistent checking the wagtail version

--- wagtail_ab_testing/test/urls.py
+++ wagtail_ab_testing/test/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail import VERSION as WAGTAIL_VERSION
 
-try:
+if WAGTAIL_VERSION >= (3, 0):
     from wagtail import urls as wagtail_urls
-except ImportError:
+else:
     from wagtail.core import urls as wagtail_urls
 
 from wagtail_ab_testing import api as ab_testing_api
