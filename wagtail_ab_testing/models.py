0104319b02b961fe9d0fead96368e19ddd4f7c27
Nick Moreton <nick.moreton@torchbox.com>
2023-03-02 13:41:13 +0000

    Extra work for the PageRevision change

--- wagtail_ab_testing/models.py
+++ wagtail_ab_testing/models.py
@@ -17,10 +17,6 @@ from wagtail.signals import page_unpublished
 
 from .events import get_event_types
 
-
-def get_revision_model():
-    return "wagtailcore.Revision"
-
 class AbTestManager(models.Manager):
     def get_current_for_page(self, page):
         return self.get_queryset().filter(page=page).exclude(status__in=[AbTest.STATUS_CANCELLED, AbTest.STATUS_COMPLETED]).first()
@@ -77,7 +73,7 @@ class AbTest(models.Model):
     page = models.ForeignKey('wagtailcore.Page', on_delete=models.CASCADE, related_name='ab_tests')
     name = models.CharField(max_length=255)
     hypothesis = models.TextField(blank=True)
-    variant_revision = models.ForeignKey(get_revision_model(), on_delete=models.CASCADE, related_name='+')
+    variant_revision = models.ForeignKey('wagtailcore.Revision', on_delete=models.CASCADE, related_name='+')
     goal_event = models.CharField(max_length=255)
     goal_page = models.ForeignKey('wagtailcore.Page', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
     sample_size = models.PositiveIntegerField(validators=[MinValueValidator(1)])
