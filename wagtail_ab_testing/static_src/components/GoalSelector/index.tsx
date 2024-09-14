b22a554d9f643bcb06fcd9423ac182746255b25f
Alex Morega <alex.morega@torchbox.com>
2024-09-14 22:58:11 +0300

    Fix page chooser (#85)

--- wagtail_ab_testing/static_src/components/GoalSelector/index.tsx
+++ wagtail_ab_testing/static_src/components/GoalSelector/index.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import getJsonScript from '../../utils/getJsonScript';
 
 // A label element without Wagtail's 'float: left;' rule applied
 const SunkenLabel = styled.label`
@@ -36,9 +37,10 @@ const GoalPageSelector: FunctionComponent<GoalPageSelectorProps> = ({
     // Fetch info about the page whenever the selected page ID is changed
     useEffect(() => {
         if (selectedPageId) {
-            fetch(
-                `${wagtailConfig.ADMIN_ROOT_URL}api/main/pages/${selectedPageId}/`,
-            )
+            const adminRootUrl = getJsonScript(
+                'wagtail-ab-testing-admin-root-url',
+            );
+            fetch(`${adminRootUrl}api/main/pages/${selectedPageId}/`)
                 .then((response) => response.json())
                 .then(setSelectedPageInfo);
         } else {
@@ -56,7 +58,7 @@ const GoalPageSelector: FunctionComponent<GoalPageSelectorProps> = ({
     ) => {
         e.preventDefault();
         (window as any).ModalWorkflow({
-            url: (window as any).chooserUrls.pageChooser,
+            url: getJsonScript('wagtail-ab-testing-choose-page-url'),
             onload: (window as any).PAGE_CHOOSER_MODAL_ONLOAD_HANDLERS,
             responses: {
                 pageChosen: function (pageData: any) {
