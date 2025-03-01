5ec5e4c2d7883a6a7c73d39058a2ca484a030714
joelwilliam2005 <joelwilliam.work@gmail.com>
2025-03-01 23:11:52 +0530

    moved creating abtest objects to setUp, removed check abtest order

--- wagtail_ab_testing/test/tests/test_delete_abtest.py
+++ wagtail_ab_testing/test/tests/test_delete_abtest.py
@@ -34,24 +34,16 @@ class TestDeleteAbTestConfirmationPage(WagtailTestUtils, TestCase):
         )
         self.page.save_revision().publish()
 
-    def test_check_ab_tests_hook_with_tests(self):
-        # Create A/B tests
-        AbTest.objects.create(
+        self.ab_test = AbTest.objects.create(
             page=self.page,
-            name="Test AB 1",
+            name="Test AB",
             first_started_at=datetime(2023, 2, 15),
             variant_revision=self.page.get_latest_revision(),
             status=AbTest.STATUS_DRAFT,
             sample_size=10,
         )
-        AbTest.objects.create(
-            page=self.page,
-            name="Test AB 2",
-            first_started_at=datetime(2023, 2, 20),
-            variant_revision=self.page.get_latest_revision(),
-            status=AbTest.STATUS_DRAFT,
-            sample_size=10,
-        )
+
+    def test_check_ab_tests_hook_with_tests(self):
         # Simulate visiting the delete page
         response = self.client.get(
             reverse("wagtailadmin_pages:delete", args=[self.page.id])
@@ -65,30 +57,13 @@ class TestDeleteAbTestConfirmationPage(WagtailTestUtils, TestCase):
         )
 
     def test_check_ab_tests_hook_without_tests(self):
+        AbTest.objects.all().delete()
         # Simulate visiting the delete page
         request = self.factory.get("/")
         response = check_ab_tests_for_page(request, self.page)
         self.assertIsNone(response)
 
     def test_ab_test_confirm_delete_view(self):
-        # Create A/B tests
-        AbTest.objects.create(
-            page=self.page,
-            name="Test AB 1",
-            first_started_at=datetime(2023, 2, 15),
-            variant_revision=self.page.get_latest_revision(),
-            status=AbTest.STATUS_DRAFT,
-            sample_size=10,
-        )
-        AbTest.objects.create(
-            page=self.page,
-            name="Test AB 2",
-            first_started_at=datetime(2023, 2, 20),
-            variant_revision=self.page.get_latest_revision(),
-            status=AbTest.STATUS_DRAFT,
-            sample_size=10,
-        )
-
         response = self.client.get(
             reverse("wagtail_ab_testing:ab_test_confirm_delete", args=[self.page.id])
         )
@@ -96,31 +71,7 @@ class TestDeleteAbTestConfirmationPage(WagtailTestUtils, TestCase):
             response, "wagtail_ab_testing/confirm_delete_ab_tests.html"
         )
 
-        # Check order of abtests
-        ab_tests = response.context["ab_tests"]
-        self.assertEqual(len(ab_tests), 2)
-        self.assertEqual(ab_tests[0].name, "Test AB 2")
-        self.assertEqual(ab_tests[1].name, "Test AB 1")
-
     def test_ab_test_delete_view(self):
-        # Create A/B tests
-        AbTest.objects.create(
-            page=self.page,
-            name="Test AB 1",
-            first_started_at=datetime(2023, 2, 15),
-            variant_revision=self.page.get_latest_revision(),
-            status=AbTest.STATUS_DRAFT,
-            sample_size=10,
-        )
-        AbTest.objects.create(
-            page=self.page,
-            name="Test AB 2",
-            first_started_at=datetime(2023, 2, 20),
-            variant_revision=self.page.get_latest_revision(),
-            status=AbTest.STATUS_DRAFT,
-            sample_size=10,
-        )
-
         response = self.client.post(
             reverse("wagtail_ab_testing:ab_test_delete", args=[self.page.id])
         )
@@ -143,22 +94,6 @@ class TestDeleteAbTestConfirmationPage(WagtailTestUtils, TestCase):
         )
 
     def test_ab_test_confirm_delete_view_bad_permissions(self):
-        AbTest.objects.create(
-            page=self.page,
-            name="Test AB 1",
-            first_started_at=datetime(2023, 2, 15),
-            variant_revision=self.page.get_latest_revision(),
-            status=AbTest.STATUS_DRAFT,
-            sample_size=10,
-        )
-        AbTest.objects.create(
-            page=self.page,
-            name="Test AB 2",
-            first_started_at=datetime(2023, 2, 20),
-            variant_revision=self.page.get_latest_revision(),
-            status=AbTest.STATUS_DRAFT,
-            sample_size=10,
-        )
         self.user.is_superuser = False
         self.user.groups.clear()
         self.user.save()
